7fca69dfc28fde92c59406d4e2d064f708d0f795
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2023-02-03 17:49:15 +0100

    chore: black fromat

--- nnactive/update_data.py
+++ nnactive/update_data.py
@@ -5,12 +5,16 @@ import json
 
 from typing import List
 
-from nnactive.data.prepare_starting_budget import make_patches_from_ground_truth, make_whole_from_ground_truth, make_empty_from_ground_truth, Patch
+from nnactive.data.prepare_starting_budget import (
+    make_patches_from_ground_truth,
+    make_whole_from_ground_truth,
+    make_empty_from_ground_truth,
+    Patch,
+)
 
 LOOP_PATTERN = "loop_"
 
 # def label_patches(patches:List[dict], dataset_dir:Path, input_dir:Path):
-    
 
 
 if __name__ == "__main__":
@@ -27,26 +31,25 @@ if __name__ == "__main__":
 
     loop_files = []
     for file in os.listdir(data_path):
-        if file[:len(LOOP_PATTERN)] == LOOP_PATTERN:
+        if file[: len(LOOP_PATTERN)] == LOOP_PATTERN:
             loop_files.append(file)
 
     loop_files.sort(key=lambda x: int(x.split(LOOP_PATTERN)[1].split(".json")[0]))
-    
+
     # Take only loop_files up to a certain loop_{loop_val}.json
     if loop_val is not None:
-        loop_files = [loop_files[i] for i in range(loop_val +1)]
-    
-    # load info 
+        loop_files = [loop_files[i] for i in range(loop_val + 1)]
+
+    # load info
     patches = []
     for loop_file in loop_files:
-        with open(data_path/loop_file, "r") as file:
+        with open(data_path / loop_file, "r") as file:
             patches_loop: List[dict] = json.load(file)["patches"]
         patches.extend(patches_loop)
 
-    with open(data_path/"dataset.json", "r") as file:
+    with open(data_path / "dataset.json", "r") as file:
         data_json = json.load(file)
 
-    
     whole_label = []
     patch_label = []
     for patch in patches:
@@ -55,21 +58,32 @@ if __name__ == "__main__":
         else:
             patch_label.append(patch)
 
-    labeled_files = set([patch["file"] for patch in (patch_label+whole_label)])
-    empty_segs = [file for file in os.listdir(labeled_path/"labelsTr") if file.endswith(data_json["file_ending"])]
+    labeled_files = set([patch["file"] for patch in (patch_label + whole_label)])
+    empty_segs = [
+        file
+        for file in os.listdir(labeled_path / "labelsTr")
+        if file.endswith(data_json["file_ending"])
+    ]
     empty_segs = [file for file in empty_segs if file not in labeled_files]
 
-    make_whole_from_ground_truth(whole_label, labeled_path/"labelsTr", data_path/"labelsTr")
+    make_whole_from_ground_truth(
+        whole_label, labeled_path / "labelsTr", data_path / "labelsTr"
+    )
 
     patch_label = Patch.from_json()
     make_patches_from_ground_truth(
-        patch_label, labeled_path/"labelsTr", data_path/"labelsTr", data_json, data_json["labels"]["ignore"]
+        patch_label,
+        labeled_path / "labelsTr",
+        data_path / "labelsTr",
+        data_json,
+        data_json["labels"]["ignore"],
     )
 
     # import IPython
     # IPython.embed()
-    make_empty_from_ground_truth(empty_segs,labeled_path/"labelsTr", data_path/"labelsTr", data_json["labels"]["ignore"])
-
-
-    
-
+    make_empty_from_ground_truth(
+        empty_segs,
+        labeled_path / "labelsTr",
+        data_path / "labelsTr",
+        data_json["labels"]["ignore"],
+    )
