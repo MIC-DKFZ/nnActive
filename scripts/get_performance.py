02f606f8447b044a553b1af0762802f10ec4e0f8
Kim-Celine Kahl <k.kahl@dkfz-heidelberg.de>
2023-02-17 16:40:34 +0100

    Save cross validation results from training in json

--- scripts/get_performance.py
+++ scripts/get_performance.py
@@ -1,7 +1,9 @@
+import json
 import os
 import subprocess
 from argparse import ArgumentParser
 from pathlib import Path
+from nnunetv2.utilities.file_path_utilities import get_output_folder
 
 from nnactive.loops.loading import get_sorted_loop_files
 from nnactive.nnunet.utils import (
@@ -25,26 +27,43 @@ def main():
     dataset_id = args.dataset_id
 
     trainer = "nnUNetDebugTrainer"
+    configuration = "3d_fullres"
     images_path = get_raw_path(dataset_id) / "imagesVal"
     labels_path = get_raw_path(dataset_id) / "labelsVal"
     loop_val = len(get_sorted_loop_files(get_raw_path(dataset_id))) - 1
     pred_path = get_results_path(dataset_id) / "predVal"
     dataset_json_path = get_raw_path(dataset_id) / "dataset.json"
-    plans_path = get_preprocessed_path(dataset_id) / "nnUNetPlans.json"
+    plans_identifier = "nnUNetPlans"
+    plans_path = get_preprocessed_path(dataset_id) / f"{plans_identifier}.json"
 
     loop_results_path: Path = (
         nnActive_results
         / convert_id_to_dataset_name(dataset_id)
         / f"loop_{loop_val:03d}"
-        / "summary.json"
     )
 
-    ex_command = f"nnUNetv2_predict -d {dataset_id} -c 3d_fullres -i {images_path} -o {pred_path} -tr {trainer}"
+    loop_summary_json = loop_results_path / "summary.json"
+    loop_summary_cross_val_json = loop_results_path / "summary_cross_val.json"
+
+    ex_command = f"nnUNetv2_predict -d {dataset_id} -c {configuration} -i {images_path} -o {pred_path} -tr {trainer}"
     subprocess.call(ex_command, shell=True)
 
-    os.makedirs(loop_results_path.parent, exist_ok=True)
-    ex_command = f"nnUNetv2_evaluate_folder -djfile {dataset_json_path} -pfile {plans_path} -o {loop_results_path} {labels_path} {pred_path}"
+    os.makedirs(loop_results_path, exist_ok=True)
+    ex_command = f"nnUNetv2_evaluate_folder -djfile {dataset_json_path} -pfile {plans_path} -o {loop_summary_json} {labels_path} {pred_path}"
     subprocess.call(ex_command, shell=True)
+
+    print("Creating a summary of the cross validation results from training...")
+    summary_cross_val_dict = {}
+    for fold in [0, 1, 2, 3, 4]:
+        trained_model_path = get_output_folder(
+            dataset_id, trainer, plans_identifier, configuration, fold
+        )
+        summary_json_train = Path(trained_model_path) / "validation" / "summary.json"
+        with open(summary_json_train, "r") as f:
+            summary_dict_train = json.load(f)
+        summary_cross_val_dict[f"fold_{fold}"] = summary_dict_train
+    with open(loop_summary_cross_val_json, "w") as f:
+        json.dump(summary_cross_val_dict, f, indent=2)
 
 
 if __name__ == "__main__":
