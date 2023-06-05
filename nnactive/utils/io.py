c23f60ea3d54c76c3c0b204fb1debe7f59fba48b
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2023-06-05 09:16:26 +0200

    bugfix: dataclass saving key error

--- nnactive/utils/io.py
+++ nnactive/utils/io.py
@@ -6,10 +6,13 @@ from pydantic import dataclasses
 
 def get_clean_dataclass_dict(data: dataclasses):
     datadict = data.__dict__
+    popkeys = []
     for key in datadict:
         if isinstance(key, str):
             if key.startswith("__") and key.endswith("__"):
-                datadict.pop(key)
+                popkeys.append(key)
+    for key in popkeys:
+        datadict.pop(key)
     return datadict
 
 
