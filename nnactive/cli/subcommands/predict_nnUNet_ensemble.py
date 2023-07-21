be0cc5d97b67e2ed7d7946cc090d78e47c4be970
Carsten Lüth <carsten.lueth@dkfz-heidelberg.de>
2023-07-21 10:29:32 +0200

    Skip pool prediction for random queries

--- nnactive/cli/subcommands/predict_nnUNet_ensemble.py
+++ nnactive/cli/subcommands/predict_nnUNet_ensemble.py
@@ -20,19 +20,19 @@ def main(args: Namespace) -> None:
 
 def predict_nnUNet_ensemble(dataset_id):
     config = ActiveConfig.get_from_id(dataset_id)
+    if not config.uncertainty.startswith("random"):
+        images_path = get_raw_path(dataset_id) / "imagesTr"
+        output_path = get_results_path(dataset_id) / "predTr"
 
-    images_path = get_raw_path(dataset_id) / "imagesTr"
-    output_path = get_results_path(dataset_id) / "predTr"
+        num_folds = config.working_folds
 
-    num_folds = config.working_folds
-
-    for fold in range(num_folds):
-        output_fold_path = output_path / f"fold_{fold}"
-        ex_command = f"nnUNetv2_predict -d {dataset_id} -c {config.model_config} -i {images_path} -o {output_fold_path} -tr {config.trainer} -f {fold} --save_probabilities {config.add_uncertainty}"
-        print(ex_command)
-        subprocess.run(
-            ex_command, shell=True, check=True
-        )  # timeout=TIMEOUT_S is not longer required due to better multiprocessing
+        for fold in range(num_folds):
+            output_fold_path = output_path / f"fold_{fold}"
+            ex_command = f"nnUNetv2_predict -d {dataset_id} -c {config.model_config} -i {images_path} -o {output_fold_path} -tr {config.trainer} -f {fold} --save_probabilities {config.add_uncertainty}"
+            print(ex_command)
+            subprocess.run(
+                ex_command, shell=True, check=True
+            )  # timeout=TIMEOUT_S is not longer required due to better multiprocessing
 
     state = State.get_id_state(dataset_id)
     state.pred_tr = True
