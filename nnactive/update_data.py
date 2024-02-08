3aec44a9fea15237f8149bc1cb824d24eb4edf8a
Carsten Lueth <carsten.lueth@dkfz-heidelberg.de>
2024-02-08 15:34:50 +0100

    clear issue regarding do_all

--- nnactive/update_data.py
+++ nnactive/update_data.py
@@ -47,6 +47,7 @@ def update_data(
             "max" if loop_val is None else loop_val, len(patches)
         )
     )
+    # by using all patches when annotated flag is true no patches can be lost.
     create_labels_from_patches(
         patches,
         ignore_label,
